4f87d1c29715fa0721de7136e3c8cdcdad757c42
Alican Beydemir <alicanbeydemir@gmail.com>
2023-01-18 21:04:11 +0300

    Refactor

--- src/main/java/com/beydemir/assignment/wscachejson/service/SubscriberService.java
+++ src/main/java/com/beydemir/assignment/wscachejson/service/SubscriberService.java
@@ -6,6 +6,8 @@ import com.beydemir.assignment.wscachejson.service.exception.SubscriberNotFoundE
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
@@ -16,17 +18,20 @@ import java.util.List;
 @Service
 public class SubscriberService {
     private final SubscriberRepository repository;
+    private CacheManager cacheManager;
     private final static Logger logger = LoggerFactory.getLogger(SubscriberService.class);
 
 
     @Autowired
-    public SubscriberService(SubscriberRepository repository) {
+    public SubscriberService(SubscriberRepository repository, CacheManager cacheManager) {
         this.repository = repository;
+        this.cacheManager = cacheManager;
     }
 
     // get all records
     public List<Subscriber> getAllSubscribers() {
         logger.info("/subscriberService getAll");
+        logger.info("Cache Status : {}", cacheManager.getCache("subscribersCache"));
         return repository.findAll();
     }
 
@@ -37,34 +42,38 @@ public class SubscriberService {
     }
 
     // inserting new record
+    @CachePut(cacheNames = {"subscribersCache"})
     public Subscriber addNewSubscriber(Subscriber subscriber) {
         Subscriber saved = repository.save(subscriber);
         logger.info("/subscriber[POST] id = {}, name = {}, msisdn = {}", saved.getId(), subscriber.getName(), subscriber.getMsisdn());
+        logger.info("Cache Status : {}", cacheManager.getCache("subscribersCache"));
         return saved;
     }
 
     // find existing record
-    @Cacheable(cacheNames = {"subscriberCache"}, key = "#subscriberId")
+    @Cacheable(cacheNames = {"subscribersCache"}, key = "#subscriberId")
     public Subscriber findSubscriber(Long subscriberId) {
-        // simulating backend querying delay
-        return repository.findById(subscriberId).get();
+        logger.info("Cache Status : {}", cacheManager.getCache("subscribersCache").getNativeCache());
+        return repository.findById(subscriberId).orElseThrow(() -> new SubscriberNotFoundException("Subscriber Not Found"));
     }
 
     // updating existing record
-    @CachePut(cacheNames = {"subscriberCache"}, key="#subscriber")
+    @CachePut(cacheNames = {"subscribersCache"}, key="#subscriber.id")
     public Subscriber updateSubscriber(Subscriber subscriber) {
         logger.info("/subscriber[PUT] id = {}, name = {}, msisdn = {}", subscriber.getId(), subscriber.getName(), subscriber.getMsisdn());
-        Subscriber subscriberRead = repository.findById(subscriber.getId()).orElseThrow(() -> new SubscriberNotFoundException("Subscriber Not Found"));
+        logger.info("Cache Status : {}", cacheManager.getCache("subscribersCache").getNativeCache());
+        Subscriber subscriberRead = findSubscriber(subscriber.getId());
         subscriberRead.setName(subscriber.getName());
         subscriberRead.setMsisdn(subscriber.getMsisdn());
         return repository.save(subscriberRead);
     }
 
     // deleting existing record
-    @CacheEvict(cacheNames = {"subscriberCache"}, key = "#subscriber")
+    @CacheEvict(cacheNames = {"subscribersCache"}, key = "#subscriber.id")
     public Subscriber deleteSubscriber(Subscriber subscriber) {
-        Subscriber subscriberRead = repository.findById(subscriber.getId()).orElseThrow(() -> new SubscriberNotFoundException("Subscriber Not Found"));
+        Subscriber subscriberRead = findSubscriber(subscriber.getId());
         logger.info("/subscriber[DELETE] id = {} ", subscriberRead.getId());
+        logger.info("Cache Status : {}", cacheManager.getCache("subscribersCache").getNativeCache());
         repository.deleteById(subscriberRead.getId());
         return subscriberRead;
     }
